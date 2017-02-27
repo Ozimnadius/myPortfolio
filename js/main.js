95ea1ef884d030b66cad1f9c6a0f0624578e2d97
Ozimnadius <79251486565@yandex.ru>
2017-02-28 01:07:44 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Михаил on 25.02.2017.
+ */
+
+window.onload = function () {
+        var auth = document.getElementsByClassName('auth');
+        auth[0].classList.add('bounceInDown');
+        auth[0].classList.add('auth_onload');
+    };
